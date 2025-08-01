12b0a2cdee17e69c5053594ad6ef94d56d60e180
eddieyg <eddiezhao@foxmail.com>
2025-08-01 15:26:45 +0800

    🦄 refactor: srcDir

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Browser Extension Starter
 
 This template should help get you started developing with React in WXT.
+
+## Features
+
+### Eslint
+antfu eslint config https://github.com/antfu/eslint-config
+
+### I18n
+
+### UI(shadcn)
+
+### Tailwind Css
+
+### Dark Mode
--- wxt.config.ts
+++ wxt.config.ts
@@ -2,5 +2,12 @@ import { defineConfig } from 'wxt'
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  srcDir: 'src',
   modules: ['@wxt-dev/module-react'],
+  webExt: {
+    // startUrls: ['https://x.com'],
+    chromiumArgs: ['--user-data-dir=./.wxt/chrome-data'],
+    openConsole: true,
+    openDevtools: true,
+  },
 })
