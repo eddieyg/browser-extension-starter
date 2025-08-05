68b0f2fcc2827960a993821d7df16f4d21b22a11
eddieyg <eddiezhao@foxmail.com>
2025-08-05 17:21:37 +0800

    ✨ feat: set wxt i18n

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,14 +5,14 @@ export default defineConfig({
   srcDir: 'src',
   modules: ['@wxt-dev/module-react'],
   webExt: {
-    // startUrls: ['https://x.com'],
     chromiumArgs: ['--user-data-dir=./.wxt/chrome-data'],
     openConsole: true,
     openDevtools: true,
   },
   manifest: {
-    name: 'Browser Extension Starter',
-    description: 'Based on WXT browser project quick starter',
+    name: '__MSG_extName__',
+    description: '__MSG_extDescription__',
+    default_locale: 'en',
     permissions: ['history', 'storage'],
     optional_permissions: ['bookmarks'],
   },
